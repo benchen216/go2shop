586ff99d309e0a73c20082a9d6faacbde277b246
benchen216 <benchen216@gmail.com>
2022-12-07 17:26:02 +0800

    del x

--- src/server/trpc/router/example.ts
+++ src/server/trpc/router/example.ts
@@ -11,14 +11,13 @@ export const exampleRouter = router({
       };
     }),
   getAll: publicProcedure.query(({ ctx }) => {
-      let x=ctx.prisma.example.create({
+      ctx.prisma.example.create({
         data: {
             id:'2',
         }
       }).finally(() => {
         console.log("xxxx");
       });
-      console.log(x);
       console.log(ctx.prisma.example.findMany());
     return ctx.prisma.example.findMany();
   }),
