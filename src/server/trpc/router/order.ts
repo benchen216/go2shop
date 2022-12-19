c70015b1752048740c5a53376170ffa0d8f76678
benchen216 <benchen216@gmail.com>
2022-12-20 07:28:01 +0800

    add year to order id

--- src/server/trpc/router/order.ts
+++ src/server/trpc/router/order.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 import fetch from "node-fetch";
-import { router, publicProcedure,protectedProcedure } from "../trpc";
+import { protectedProcedure, router } from "../trpc";
 
 export const  orderRouter  = router({
   checkout: protectedProcedure
@@ -27,7 +27,7 @@ export const  orderRouter  = router({
     }))
     .mutation(async ({ctx, input }) => {
       const now = new Date();
-      const number = '' + now.getMonth() + now.getDate() + (now.getTime()%(24*60*60*1000)) + Math.floor(Math.random()*10);
+      const number = '' +now.getFullYear().toString().slice(2,4)+ now.getMonth() + now.getDate() + (now.getTime()%(24*60*60*1000)) + Math.floor(Math.random()*10);
       const postData = {
         'prime': input.prime,
         'partner_key':
@@ -43,7 +43,7 @@ export const  orderRouter  = router({
         },
         'remember': false,
       };
-      const ans=await fetch("https://sandbox.tappaysdk.com/tpc/payment/pay-by-prime",
+      return await fetch("https://sandbox.tappaysdk.com/tpc/payment/pay-by-prime",
         {
           method: 'POST',
           headers: {
@@ -52,44 +52,43 @@ export const  orderRouter  = router({
           },
           body: JSON.stringify(postData)
         }).then(async (response) => {
-          const data = await response.json();
-          console.log(data);
+        const data = await response.json();
+        console.log(data);
 
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          //@ts-ignore
-          if(data.status === 0){
-            const order = ctx.prisma.order.create({
-              data: {
-                orderId: number,
-                prime: input.prime,
-                name: input.name,
-                phone: input.phone,
-                email: input.email,
-                address: input.address,
-                total: input.total,
-                shipping: input.shipping,
-                payment: input.payment,
-                detail: input.cart,
-                userId: Number(ctx.session.user.id),
-              }
-            })
-            order.then(
-              (res) => {
-                console.log(res);
-                return {result:"success"};
-              }
-            ).catch(
-              (err) => {
-                console.log(err);
-                return {result:"fail"};
-              }
-            )
-            return {result:"success"};
-          }else{
-            return {result:"fail"};
-          }
-        });
-      return ans;
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        //@ts-ignore
+        if (data.status === 0) {
+          const order = ctx.prisma.order.create({
+            data: {
+              orderId: number,
+              prime: input.prime,
+              name: input.name,
+              phone: input.phone,
+              email: input.email,
+              address: input.address,
+              total: input.total,
+              shipping: input.shipping,
+              payment: input.payment,
+              detail: input.cart,
+              userId: Number(ctx.session.user.id),
+            }
+          })
+          order.then(
+            (res) => {
+              console.log(res);
+              return { result: "success" };
+            }
+          ).catch(
+            (err) => {
+              console.log(err);
+              return { result: "fail" };
+            }
+          )
+          return { result: "success" };
+        } else {
+          return { result: "fail" };
+        }
+      });
       //Todo: check cart items fit the product
     }),
 });
