de0bf5016b7ca6f22ddbc21aad22f4f4b1e2d1c9
benchen216 <benchen216@gmail.com>
2022-12-21 01:06:33 +0800

    del unused config

--- next.config.mjs
+++ next.config.mjs
@@ -13,15 +13,5 @@ const config = {
     locales: ["en","zh-TW"],
     defaultLocale: "zh-TW",
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'tailwindui.com',
-        port: '',
-        pathname: '/img/**',
-      },
-    ],
-  },
 };
 export default config;
